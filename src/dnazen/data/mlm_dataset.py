fix: missing ngram attention mask

src/dnazen/data/mlm_dataset.py
@@ -2,12 +2,15 @@ from typing import TypedDict
 import random
 import os
 import json
+import logging
 
 import torch
 from torch.utils.data import Dataset
 from transformers import PreTrainedTokenizer, AutoTokenizer
 
 from dnazen.ngram import NgramEncoder
+
+logger = logging.getLogger(__name__)
 
 
 class MlmData(TypedDict):
@@ -61,6 +64,7 @@ class MlmDataset(Dataset):
         ngram_encoder: NgramEncoder,
         core_ngrams: set[tuple[int, ...]],
         mlm_prob: float = 0.15,
+        verbose: bool = True,
     ):
         """Initialize a Mlm dataset.
 
@@ -70,6 +74,7 @@ class MlmDataset(Dataset):
             tokenizer (PreTrainedTokenizer): tokenizer from `transformers`.
             ngram_encoder (NgramEncoder): a ngram encoder for encoding.
             mlm_prob (float, optional): The proportion of masked tokens. Defaults to 0.15.
+            verbose (bool, optional): Whether initialize with loggings. Defaults to True.
         """
         super().__init__()
         self.ngram_encoder = ngram_encoder
@@ -87,12 +92,19 @@ class MlmDataset(Dataset):
         for ngram in core_ngrams:
             self.core_ngram_min_len = min(self.core_ngram_min_len, len(ngram))
             self.core_ngram_max_len = max(self.core_ngram_max_len, len(ngram))
-        print("minimum core ngram len=", self.core_ngram_min_len)
-        print("maximum core ngram len=", self.core_ngram_max_len)
+        # print("minimum core ngram len=", self.core_ngram_min_len)
+        # print("maximum core ngram len=", self.core_ngram_max_len)
 
         self.mlm_prob = mlm_prob
         self.tokens = tokens
         self.attn_mask = attn_mask
+        
+        if verbose:
+            logger.info(
+                f"MlmDataset initialized. "
+                f"minimum core ngram len={self.core_ngram_min_len}; "
+                f"maximum core ngram len={self.core_ngram_max_len}"
+            )
 
     @property
     def ngram_vocab_size(self):
@@ -131,6 +143,9 @@ class MlmDataset(Dataset):
         )
 
     def save(self, save_dir: str):
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+            print(f"Warning: Directory {save_dir} did not exist and was created.")
         ngram_encoder_path = os.path.join(save_dir, self.NGRAM_ENCODER_FNAME)
         data_path = os.path.join(save_dir, self.DATA_FNAME)
         core_ngram_path = os.path.join(save_dir, self.CORE_NGRAMS_FNAME)
@@ -158,7 +173,7 @@ class MlmDataset(Dataset):
         tokenizer_path = os.path.join(save_dir, cls.TOKENIZER_DIR)
         data_path = os.path.join(save_dir, cls.DATA_FNAME)
         core_ngram_path = os.path.join(save_dir, cls.CORE_NGRAMS_FNAME)
-        data = torch.load(data_path)
+        data = torch.load(data_path, weights_only=True)
 
         # tokenizer = PreTrainedTokenizer.from_pretrained(tokenizer_path, local_files_only=True)
         tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
@@ -175,7 +190,7 @@ class MlmDataset(Dataset):
     def __len__(self):
         return self.tokens.size(0)
 
-    def __getitem__(self, index):
+    def __getitem__(self, index) -> MlmData:
         # do masking during run-time
         input_ids_, labels_ = MlmDataset._create_mlm_predictions(
             token_seq=self.tokens[index],
@@ -198,6 +213,7 @@ class MlmDataset(Dataset):
             "input_ids": input_ids_,
             "labels": labels_,
             "attention_mask": self.attn_mask[index],
+            "ngram_attention_mask": ngram_encoder_outputs["ngram_attention_mask"],
             "ngram_input_ids": ngram_encoder_outputs["ngram_ids"],
             "ngram_position_matrix": ngram_encoder_outputs["ngram_position_matrix"],
         }
@@ -210,7 +226,7 @@ class MlmDataset(Dataset):
         sep_token: int,
         mask_token: int,
         vocab_list: list[int],
-        core_ngrams: set[tuple[int, ...]],  # TODO: enable this
+        core_ngrams: set[tuple[int, ...]],
         min_core_ngram_len: int,
         max_core_ngram_len: int,
     ) -> tuple[torch.Tensor, torch.Tensor]:
@@ -229,7 +245,8 @@ class MlmDataset(Dataset):
             .squeeze()
             .tolist()
         )
-
+        
+        # get non-candidate indexes
         token_seq_list = token_seq.tolist()
         non_candidiate_idxes = []
         for idx in candidate_idxes_list:
@@ -240,10 +257,13 @@ class MlmDataset(Dataset):
                 if tuple(token_seq_list[idx : idx + len_]) in core_ngrams:
                     non_candidiate_idxes += range(idx, idx + len_)
 
+        seq_len = len(candidate_idxes_list)
         candidate_idxes = torch.tensor(
             [idx for idx in candidate_idxes_list if idx not in non_candidiate_idxes],
             dtype=torch.int32,
         )
+        len_prop = seq_len / len(candidate_idxes)
+        mlm_prob *= len_prop # modify the mlm prob
 
         masked_token_seq = token_seq.clone()
         # labels = torch.zeros_like(token_seq)
@@ -256,7 +276,7 @@ class MlmDataset(Dataset):
         # Sample which tokens to mask
         mask_prob = torch.full_like(token_seq, mlm_prob, dtype=torch.float)
         mask_prob[~candidate_mask] = 0
-        mask_mask = torch.bernoulli(mask_prob).bool()
+        mask_mask = torch.bernoulli(mask_prob).bool() # 1 = sample; 0 = not sample
 
         # Apply masking
         labels[mask_mask] = masked_token_seq[mask_mask]
@@ -268,7 +288,10 @@ class MlmDataset(Dataset):
         masked_token_seq[mask_mask_80] = MASK
         masked_token_seq[mask_mask_10] = token_seq[mask_mask_10]
         masked_token_seq[mask_mask_10_rand] = torch.tensor(
-            random.choices(vocab_list, k=mask_mask_10_rand.sum().item()),
+            random.choices(
+                vocab_list, 
+                k=int(mask_mask_10_rand.sum().item())
+            ),
             dtype=torch.long,
         )
 

src/dnazen/ngram/_ngram_encoder.py
@@ -16,6 +16,7 @@ class _NgramEncoderConfig(TypedDict):
 
 class EncodedNgram(TypedDict):
     ngram_ids: torch.Tensor
+    ngram_attention_mask: torch.Tensor
     ngram_position_matrix: torch.Tensor
 
 
@@ -38,6 +39,28 @@ class NgramEncoder:
 
     def _get_ngram_id(self, tokens: tuple[int, ...]) -> int | None:
         return self._vocab.get(tokens, None)
+    
+    def get_num_matches(
+        self, token_ids: torch.Tensor, pad_token_id: int = 3
+    ):
+        """Calculate the number of ngram matches from token_ids."""
+        assert token_ids.dim() == 1, f"token_ids should have dim=1, but got {token_ids.dim()}"
+
+        token_len = token_ids.shape[0]
+        token_ids = token_ids[token_ids != pad_token_id]
+        token_ids_list = token_ids.tolist()
+        # token_len = len(token_ids_list)
+
+        num_matches = 0
+        for ngram_len in range(self._min_ngram_len, self._max_ngram_len):
+            for q in range(token_len - ngram_len + 1):
+                ngram: tuple[int, ...] = tuple(token_ids_list[q : q + ngram_len])
+                ngram_id = self._get_ngram_id(ngram)
+                if ngram_id is None:
+                    continue
+                # found the match
+                num_matches += 1
+        return num_matches
 
     def encode(self, token_ids: torch.Tensor, pad_token_id: int = 3) -> EncodedNgram:
         """
@@ -49,15 +72,15 @@ class NgramEncoder:
         """
         assert token_ids.dim() == 1
 
+        token_len = token_ids.shape[0]
+        token_ids = token_ids[token_ids != pad_token_id]
         token_ids_list = token_ids.tolist()
-        token_len = len(token_ids_list)
-        # Trim trailing pad tokens
-        while token_ids_list and token_ids_list[-1] == pad_token_id:
-            token_ids_list.pop()
+        # token_len = len(token_ids_list)
 
         _cur_ngram_id_idx = 0  # index to ngram_ids
         ret_val: EncodedNgram = {
             "ngram_ids": torch.zeros(self._max_ngrams, dtype=torch.int),
+            "ngram_attention_mask": torch.zeros(self._max_ngrams, dtype=torch.int),
             "ngram_position_matrix": torch.zeros(
                 token_len, self._max_ngrams, dtype=torch.bool
             ),
@@ -71,6 +94,7 @@ class NgramEncoder:
                     continue
                 # found the match
                 ret_val["ngram_ids"][_cur_ngram_id_idx] = ngram_id
+                ret_val["ngram_attention_mask"][_cur_ngram_id_idx] = 1
                 ret_val["ngram_position_matrix"][
                     q : q + ngram_len, _cur_ngram_id_idx
                 ] = 1
@@ -78,6 +102,36 @@ class NgramEncoder:
                 if _cur_ngram_id_idx >= self._max_ngrams:
                     return ret_val
         return ret_val
+
+    def set_max_ngram_match(self, max_ngrams: int):
+        self._max_ngrams = max_ngrams
+    
+    @classmethod
+    def from_list(
+        cls, 
+        ngram_list: list[list[int]], 
+        max_ngrams: int = 20
+    ):
+        """Instantiate a new :class:`~dnazen.ngram.NgramTokenizer` from list of ngram list.
+
+        Do not question why this method is even here. It is 20:00 and the day after tmw is Spring Festival.
+        It's the only and least ugly way to finish my one-time-only-but-very-urgent job.
+        """
+        vocab_dict = {}
+        min_ngram_len = 100
+        max_ngram_len = 0
+        for idx, vocab in enumerate(ngram_list):
+            k_ = tuple(vocab)
+            min_ngram_len = min(len(k_), min_ngram_len)
+            max_ngram_len = max(len(k_), max_ngram_len)
+            vocab_dict[k_] = idx
+
+        return cls(
+            vocab_dict=vocab_dict,
+            min_ngram_len=min_ngram_len,
+            max_ngram_len=max_ngram_len,
+            max_ngrams=max_ngrams,
+        )
 
     @classmethod
     def from_file(cls, path):
@@ -159,6 +213,6 @@ class NgramEncoder:
         """
         Get the unique identifier of ngram encoder.
 
-        The identifier is soloy based on the ngram vocabulary.
+        The identifier is solely based on the ngram vocabulary.
         """
         return hash(frozenset(self._vocab.items()))