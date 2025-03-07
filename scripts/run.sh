bc6767dddc7a90da22cb651400ad47d64f2f20e5
zark <zhu04303661@163.com>
2025-03-07 17:08:32 +0800

    merge form origin/zeq-harnvo-patch

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,7 @@
 
 
  #./prepare_data_and_config.sh --train-ngram    --experiment 1
- ./prepare_data_and_config.sh --coverage-analysis   --experiment 1
+ #./prepare_data_and_config.sh --coverage-analysis   --experiment 1
 
 
 
@@ -23,4 +23,4 @@
 #./prepare_data_and_config.sh --tokenize-train   
 #./prepare_data_and_config.sh --tokenize-dev   
 
-#./prepare_data_and_config.sh --prepare-dataset   --experiment 3
+./prepare_data_and_config.sh --prepare-dataset   --experiment 1
